f3d16b319b24db7d88db42207939c4721a99d296
OrlandoTellez <espadachinkinno@gmail.com>
2025-12-08 01:28:31 -0600

    feat: agregar rutas privadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { DashboardLayout } from './components/layout/DashboardLayout'
+import { Outlet } from 'react-router-dom'
+
+const queryClient = new QueryClient()
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <QueryClientProvider client={queryClient}>
+        <DashboardLayout>
+          <Outlet />
+        </DashboardLayout>
+      </QueryClientProvider>
     </>
   )
 }
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,24 @@
 import { Route, Routes } from "react-router-dom"
 import { LoginForm } from "../components/auth/LoginForm"
+import App from "../App"
+import { PrivateRoute } from "./PrivateRoutes"
+import { Dashboard } from "../pages/Dashboard"
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/auth/login" element={<LoginForm />} />
+
+            <Route element={<App />}>
+                <Route
+                    path="/"
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+            </Route>
         </Routes>
     )
 }
